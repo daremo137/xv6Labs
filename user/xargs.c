aad614662a7fe8c01a694fa292607f7392b097e2
daremo <1371275205@qq.com>
2024-02-04 06:14:56 +0800

    updated

--- user/xargs.c
+++ user/xargs.c
@@ -3,43 +3,20 @@
 
 char buf[1024+3], cmd[1024+3];
 
-
-int main(int argc, char *args[]){
-	int i, j;
-	char *p;
-
-	printf("this process has %d arg, the first arg is %s\n", argc, args[0]);
-	printf("all args below:\n");
-	for(i = 0; i < argc; ++ i){
-		printf("%s ", args[i]);
-	}
-	printf("\n");
+int main(int argc, char *argv[]){
 	if(argc < 2){
 		printf("too few arguments for xargs\n");
 		exit(1);
 	}
 	char **cmds = (char**)malloc((argc + 2) * sizeof(char*));
 	for(int i = 0; i < argc; ++ i){
-		
+		cmds[i] = argv[i];
 	}
-	//fgets(buf, sizeof(buf), 0);
 	read(0, buf, sizeof(buf));
-	p = cmd;
-	//printf("received string is %s\n", buf);
-	for(i = 2; i < argc; ++ i){
-		//strcat(cmd, args[i]);
-		for(j = 0; args[i][j] != '\0'; ++ j){
-			*p++ = args[i][j];
-		}
-		*p++ = ' ';
-	}
-	//strcat(cmd, buf);
-	for(i = 0; buf[i] != '\0'; ++ i){
-		*p++ = buf[i];
-	}
-	*p = '\0';
-	printf("whole cmd is %s\n", cmd);
-	exec(args[1], cmds + 1);
+	cmds[argc] = buf;
+	cmds[argc + 1] = argv[argc];
+	exec(argv[1], cmds + 1);
 
+	free(cmds);
 	exit(0);
 }
